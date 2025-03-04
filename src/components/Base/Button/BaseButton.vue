f0ded4f11c7dc6bcdd85dc23d412b39edf4ceb63
Alexander Chernakov <a.chernakov@meta.studio>
2025-03-04 18:31:33 +0300

    feat: create base button

---
+++ src/components/Base/Button/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button type="button" :disabled="disabled">
+    <BaseButtonTemplate :icon="icon" :icon-class="iconClass" :disabled="disabled">
+      <slot></slot>
+    </BaseButtonTemplate>
+  </button>
+</template>
+
+<script setup lang="ts">
+import BaseButtonTemplate from "@/components/Base/ButtonTemplate/BaseButtonTemplate.vue";
+
+interface Props {
+  icon?: string
+  iconClass?: string
+  disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  iconClass: 'icon32',
+  disabled: false,
+})
+</script>
+
+<style scoped lang="scss"></style>
