312943130b585bf63d348b661b9cb80ea4f5818c
Alexander Chernakov <a.chernakov@meta.studio>
2025-03-04 18:32:22 +0300

    feat(BaseLink): create base link

---
+++ src/components/Base/Link/BaseLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="link">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.link {
+  width: max-content;
+  color: $accent-light;
+  transition: color .3s ease;
+  cursor: pointer;
+
+  @media screen and (hover: hover) {
+    &:hover {
+      color: $white;
+    }
+  }
+
+  &:active {
+    color: $white;
+  }
+}
+</style>
